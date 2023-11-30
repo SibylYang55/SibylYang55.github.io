7b39e6788206f9428afcde735ed0635d2724fece
Sibyl Yang <121019163+SibylYang55@users.noreply.github.com>
2023-11-30 21:07:35 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "sibylyang55.github.io",
+    baseUrl: "sibylyang55.github.io/美丽",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
