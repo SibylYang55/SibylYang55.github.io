d4f4427413cb5e8c22aa82597c668c0ca59f82c1
Sibyl Yang <121019163+SibylYang55@users.noreply.github.com>
2023-12-01 20:58:20 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🦄SIBYLYANG",
+    pageTitle: "SIBYLYANG",
     enableSPA: true,
     enablePopovers: false,
     analytics: {
