352a052425a6e385fa94a1b9a284a86cefce1927
Sibyl Yang <121019163+SibylYang55@users.noreply.github.com>
2023-12-02 14:22:30 +0800

    Update PageList.tsx

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -56,18 +56,6 @@ export function PageList({ cfg, fileData, allFiles, limit }: Props) {
                   </a>
                 </h3>
               </div>
-              <ul class="tags">
-                {tags.map((tag) => (
-                  <li>
-                    <a
-                      class="internal tag-link"
-                      href={resolveRelative(fileData.slug!, `tags/${tag}` as FullSlug)}
-                    >
-                      #{tag}
-                    </a>
-                  </li>
-                ))}
-              </ul>
             </div>
           </li>
         )
